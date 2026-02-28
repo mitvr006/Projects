ecfe1d339610e3df4023fab1af05fc9fc293802e
mitvr006 <pawankumar3439744@gmail.com>
2026-02-28 20:08:26 +0530

    Added company and Medicine models, configured migrations and registered models in admin panel

--- medicines/models.py
+++ medicines/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return self.name

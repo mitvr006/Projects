18b2a1efbf84a4489e64ffee08ad45172d1253bc
mitvr006 <pawankumar3439744@gmail.com>
2026-03-20 20:33:39 +0530

    fix

--- medical_management/settings.py
+++ medical_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c#qwdd3g86so-hab(6&*w8f$n%ln&z09!uk%phyhb=3y_x5uec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

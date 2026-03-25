aed5189743ab6fe68af13dfc547c488a7a36282b
mitvr006 <pawankumar3439744@gmail.com>
2026-03-25 22:28:47 +0530

    added dashboard quick buttons

--- medicines/urls.py
+++ medicines/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('companies/', views.company_list, name='company_list'),
     path('companies/add/', views.company_create, name='company_create'),
     path('companies/edit/<int:pk>/', views.company_update, name='company_update'),
